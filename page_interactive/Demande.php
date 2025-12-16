868f596f9fb57c7bbcca8357c126d2f077f0995e
Salim <unfunded0419@proton.me>
2025-12-17 00:22:51 +0100

    fusionné quelques trucs, comme la page consultation des demandes et la page des nouvelles demandes

--- page_interactive/Demande.php
+++ page_interactive/Demande.php
@@ -1,54 +1,57 @@
-
 <?php
 session_start();
+// je me suis permis de prendre tout ton HTML et je l'ai mit dans le .HTML associé (pour simplifier la tache pour le css sinon je comprenais plus rien)
 if (!isset($_SESSION["connecté"]) || $_SESSION["role"] != "etudiant" || !isset($_SESSION["id_projet"])) {
     header("Location: connexion.php");
     exit();
 }
 
-
-
 include("Connexion_DB.php");
-    $id_projet = $_SESSION["id_projet"];
-    $matricule = $_SESSION["matricule"];
+$id_projet = $_SESSION["id_projet"];
+$matricule = $_SESSION["matricule"];
 
-    $requetesql_nom_projet = "SELECT * FROM projet WHERE projet.ID_Projet = '$id_projet' ";
-    $resultat_nom_projet  = mysqli_query($conn,$requetesql_nom_projet);
-    $ligne = mysqli_fetch_assoc($resultat_nom_projet);
-    $nom_projet = $ligne['Nom']; //je récupère le nom du projet associé a l'id juste pour faire un truc propre dans le html
+//je récupère des infos pour le .html (pour faire un truc clean) 
+$requetesql_nom_projet = "SELECT * FROM projet WHERE projet.ID_Projet = '$id_projet' ";
+$resultat_nom_projet  = mysqli_query($conn,$requetesql_nom_projet);
+$ligne = mysqli_fetch_assoc($resultat_nom_projet);
+$nom_projet = $ligne['Nom']; 
 
-    $requetesql_liste_demandes = "SELECT * FROM emprunt
-                                  WHERE emprunt.ID_Projet= '$id_projet' " ;
-    $resultat_liste_demandes = mysqli_query($conn,$requetesql_liste_demandes);// je stock dans une variable toutes les demandes du projet pour les cout dans le .html
-    $nbre_lignes_liste_demandes = mysqli_num_rows($resultat_liste_demandes); // utile aussi pour cout de le .html ( si pas de ligne je mets juste "aucune demande en cours")
-
-if (isset($_POST["submit"])) {
-
-    $statut = "en attente";
-
-    $_Article = filter_input(INPUT_POST, "nom_article", FILTER_SANITIZE_SPECIAL_CHARS); 
-
-    $_date_debut = $_POST["date_debut"]; 
-
-    $_date_retour = $_POST["date_retour"]; 
-
-    $_Raison = filter_input(INPUT_POST, "raison_emprunt", FILTER_SANITIZE_SPECIAL_CHARS);
-
-    $_chercheur = "SELECT ID_Modele from modele WHERE Reference = '$_Article'"; 
-    $resultat_id_Modele = mysqli_query($conn,$_chercheur);
-    $ligne_modele = mysqli_fetch_assoc($resultat_id_Modele);
-    $id_modele= $ligne_modele['ID_Modele']; // Pour récupérer l'id du modele concerné
-
-    $requetesql_ajout_emprunt = "INSERT INTO emprunt (Date_debut, Date_fin_prevue, Raison_Emprunt, E_Matricule, ID_Projet, Statut,ID_Modele_demande) 
-                                VALUES ('$_date_debut', '$_date_retour', '$_Raison','$matricule','$id_projet','$statut','$id_modele')";
-    
-    if(mysqli_query($conn, $requetesql_ajout_emprunt)) {
+$requetesql_liste_demandes = "SELECT * FROM emprunt WHERE emprunt.ID_Projet= '$id_projet' " ;
+$resultat_liste_demandes = mysqli_query($conn,$requetesql_liste_demandes);
+$nbre_lignes_liste_demandes = mysqli_num_rows($resultat_liste_demandes); 
+if (isset($_POST["submit"])) { /* code déguelasse vérifiant si l'utilisateur à bien remplit les champs pour les dates de manière logique , ensuite il vérifie si l'objet demandé se trouve dans la db et enfin seulement l'emprunt et la table concerner sont mise à jour*/
+    $_Article = filter_input(INPUT_POST, "nom_article", FILTER_SANITIZE_SPECIAL_CHARS);  
+    $today = date('Y-m-d'); 
+        if($_POST["date_debut"] >= $today) {
+            $_date_debut = $_POST["date_debut"]; 
+                if($_POST["date_retour"] >= $_date_debut) {
+                    $_date_retour = $_POST["date_retour"]; 
+                    $_Raison = filter_input(INPUT_POST, "raison_emprunt", FILTER_SANITIZE_SPECIAL_CHARS);
+                    
+                    // j'ai corrigé le truc dont on parlait j'ai donc ajouté un champ dans la db, ça permet de ne pas passer par concerner
+                    $sql = "SELECT ID_Modele from modele WHERE Reference = '$_Article'"; 
+                    $result = mysqli_query($conn, $sql);
+                    
+                        if(mysqli_num_rows($result)>0) {
+                            $ligne_modele = mysqli_fetch_assoc($result);
+                            $id_modele = $ligne_modele['ID_Modele'];
+                            
+                            
+                            $sql = "INSERT INTO emprunt(Date_debut, Date_fin_prevue, Raison_Emprunt, E_Matricule, ID_Projet, Statut, ID_Modele_demande) VALUES ('$_date_debut', '$_date_retour', '$_Raison', '$matricule', '$id_projet', 'en attente', '$id_modele')"; /*il faut encore ajouter le projet auquel l'emprunt est associé*/ 
+                        if(mysqli_query($conn, $sql)) {
+                                    
+                                }
+                        } else {
+                            $erreur = "L'article recherché n'existe pas !"; 
+                        }
+                    
+                } else {
+                    $erreur = "On ne peut pas finir un emprunt avant de l'avoir commencer !"; 
+                }
+        } else {
+        $erreur = "L'emprunt sera autorisé au plus tôt aujourd'hui !";   
+        }
     }
-    else {
-        $erreur = "Modification de la base de donnée impossible"; 
-    } 
-
-}
-
-include '../page_html/Demande.html'; 
+include '../page_html/Demande.html';
+mysqli_close($conn);
 ?>
--- page_interactive/Stocks.php
+++ page_interactive/Stocks.php
@@ -15,7 +15,7 @@ $reuse_result = mysqli_fetch_all($result, MYSQLI_ASSOC);
 <html>
 <head>
     <meta charset="UTF-8">
-    <link rel = "stylesheet" href = "../css/Stocks.css?v=1">
+    <link rel = "stylesheet" href = "../page_html/Stocks.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Elms+Sans:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
@@ -59,10 +59,9 @@ if (isset($_POST["submit"])) {
     <?php }?>
     </main>  
     <footer>
-                <p> Page <a href = "../page_interactive/Home.php"> d'acceuil</a> </p>
-                <p> - Faire une <a href = "../page_interactive/Demande.php"> demande </a></p>
-                <p> - Consulter <a href = "../page_interactive/Stocks.php"> les stocks </a></p>
-                <p> - Suivre les <a href = "../page_interactive/Suivi_demande.php"> demandes </a></p>
+                <p> Revenir à la page <a href = "../page_interactive/Home_Projet.php"> d'acceuil</a> </p>
+                <p> Allez sur la page <a href = "../page_interactive/Demande.php"> de demande </a></p>
+                <p> Allez sur la page de consultation <a href = "../page_interactive/Stocks.php"> des stocks </a></p>
     </footer>
 </body>
 </html>
