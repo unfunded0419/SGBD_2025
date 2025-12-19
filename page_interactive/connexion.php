6e493fae9f265105754614610c642acf7ad199f0
Ratosaure <gwendehon4@gmail.com>
2025-12-19 02:05:25 +0100

    Modification de la page de connexion de manière à ce que l'on puisse choisir de se connecter en tant qu'étudiant ou responsable + modification de l'interface d'affichage des erreurs

--- page_interactive/connexion.php
+++ page_interactive/connexion.php
@@ -1,10 +1,12 @@
 <?php
     session_start(); 
+    $erreur = "";
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $_Matricule = $_POST["Matricule"]; 
         $_MDP = $_POST["MDP"];  
         if (preg_match('/^\d{6}$/', $_Matricule) ) { /*permet de vérifier si $_Matricule est une chaîne de 6 entier // = limiteur du motf, ^ = début chaîne, $ = fin chaîne, \d = char est un entier, {6} = \d répété six fois */ 
             include 'Connexion_DB.php'; 
+            if ($_POST["Responsable_Etudiant"] == "Responsable") {
             $sql = "SELECT * FROM responsable_des_equipements WHERE responsable_des_equipements.RE_Matricule = '$_Matricule' AND responsable_des_equipements.Statut = 'approuve'"; 
             $result = mysqli_query($conn, $sql); 
             if (mysqli_num_rows($result)>0){
@@ -22,7 +24,11 @@
                } else {
                 $erreur = "Mot de passe invalide"; 
                }
+            } else {
+                mysqli_close($conn);   
+                $erreur = "Utilisateur non enregistré dans la DB ou compte inactif";  
             }
+        } else {
             $sql = "SELECT * FROM etudiant WHERE etudiant.E_Matricule = '$_Matricule'"; 
             $result = mysqli_query($conn, $sql); 
                 if (mysqli_num_rows($result)>0) {
@@ -46,42 +52,50 @@
                 }else {
                     mysqli_close($conn);   
                     $erreur = "Utilisateur non enregistré dans la DB"; 
-                }  
+                } 
+            } 
         } else { 
             $erreur = "Un matricule contient 6 chiffres";
         }
-}
+    }
 ?>
 <!DOCTYPE html>
 <html lang="fr">
     <head>
         <meta charset="utf-8">
         <title>Connexion - Université de Mons </title>
-        <link rel="stylesheet" href="../css/connexion.css?v=1.1">
+        <link rel="stylesheet" href="../css/connexion.css?v=1.0">
         <link href='https://cdn.boxicons.com/3.0.3/fonts/basic/boxicons.min.css' rel='stylesheet'>
     </head>
     <body>
     <div class="boite-login">
+        <?php if(empty($erreur)) {?>
         <form action="connexion.php" method="post">
             <h1>Login</h1>
-
         <div class="entree-login">
             <input type="number" name="Matricule" placeholder="Matricule" required>
             <i class='bx  bxs-user'    ></i> 
         </div> 
-            <?php if(!empty($erreur)) {?>
-            <p> <?php echo $erreur; ?>
-            <?php } ?>
         <div class="entree-login">
             <input type="password" name="MDP" placeholder="Mot de passe" required>
             <i class='bx  bxs-lock'    ></i> 
         </div>
-
+        <div class="radio">
+            <label for = "Etudiant"> Etudiant </label>
+            <input type = "radio" id = "Etudiant" name="Responsable_Etudiant" value = "Etudiant" checked>
+            <label for = "Responsable"> Responsable </label>
+            <input type = "radio" id = "Responsable" name="Responsable_Etudiant" value = "Responsable">
+        </div> 
             <button type="submit" class="bouton-login">Connexion</button>
 
         <div class="register">
             <p> Pas encore de compte ? <a href="register.php">S'enregistrer </a> </p>
         </div>
+        <?php } else {?>
+            <div class = "error">
+        <p> <?php echo $erreur; ?> </p>
+            </div>
+        <?php } ?>
         </form>
         </div>     
     </body>
