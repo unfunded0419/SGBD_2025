c20b9a973fd3e28361dd321e998468206a94b8e4
noeeeeeeh <gerardnoe45@gmail.com>
2025-12-19 11:47:53 +0100

    choix de la connexion etudiante ou respo

--- page_interactive/connexion.php
+++ page_interactive/connexion.php
@@ -3,69 +3,74 @@ session_start();
 include 'Connexion_DB.php'; 
 $message = "";
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $matricule = $_POST['Matricule']; 
+if ($_SERVER['REQUEST_METHOD'] === 'POST') 
+{   $matricule = $_POST['Matricule']; 
     $password = $_POST['MDP'];
 
     // --- ÉTAPE 1 : VÉRIFIER DANS LA TABLE RESPONSABLE (Version MySQLi) ---
     // On prépare la requête (sécurité)
-    $stmt = $conn->prepare("SELECT * FROM responsable_des_equipements WHERE RE_Matricule = ?");
-    $stmt->bind_param("i", $matricule); // "i" veut dire integer (entier)
-    $stmt->execute();
-    $result = $stmt->get_result();
-    $admin = $result->fetch_assoc();
-    $j=0;
 
-    if ($admin && password_verify($password, $admin['Mot_de_passe'])) 
+    if(!empty($_POST["Etudiant_Responsable"]))
     {
-        session_regenerate_id(true); 
-        $_SESSION['Matricule'] = $admin['RE_Matricule'];
-        $_SESSION['pseudo'] = $admin['Nom'];
+        if ($_POST["Etudiant_Responsable"]==="Responsable") 
+        {
+            $stmt = $conn->prepare("SELECT * FROM responsable_des_equipements WHERE RE_Matricule = ?");
+            $stmt->bind_param("i", $matricule); // "i" veut dire integer (entier)
+            $stmt->execute();
+            $result = $stmt->get_result();
+            $admin = $result->fetch_assoc();
 
-        if ($admin['admin'] == 1) {
-            $_SESSION['role'] = 'admin'; 
-            $_SESSION['logged'] = true;
-            header('Location: Home.php');
-            exit();
-        } elseif ($admin['Statut'] == "Accepté") {
-            $_SESSION['role'] = 'resp'; 
-            $_SESSION['logged'] = true;
-            header('Location: Home.php');
-            exit();
-        } else{
-            $attente = "Validation en attente.";
-            $j=1;
+            if($admin && password_verify($password, $admin['Mot_de_passe']))
+            {
+                session_regenerate_id(true); 
+                $_SESSION['Matricule'] = $admin['RE_Matricule'];
+                $_SESSION['pseudo'] = $admin['Nom'];
+
+                if ($admin['admin'] == 1) {
+                    $_SESSION['role'] = 'admin'; 
+                    $_SESSION['logged'] = true;
+                    header('Location: Home.php');
+                    exit();
+                } elseif ($admin['Statut'] == "Accepté") {
+                    $_SESSION['role'] = 'resp'; 
+                    $_SESSION['logged'] = true;
+                    header('Location: Home.php');
+                    exit();
+                } elseif($admin['Statut'] == "En attente"){
+                    $attente = "Validation en attente.";
+                }
+            }else{
+                    $erreur = "Identifiants non existants.";
+                }
         }
         
-    }
-    
-    // --- ÉTAPE 2 : VÉRIFIER DANS LA TABLE ETUDIANT ---
-    else {
-        // On ferme la requête précédente
-        $stmt->close();
+        // --- ÉTAPE 2 : VÉRIFIER DANS LA TABLE ETUDIANT ---
+        elseif($_POST["Etudiant_Responsable"]==="Etudiant")
+        {
+            $stmt = $conn->prepare("SELECT * FROM etudiant WHERE E_Matricule = ?");
+            $stmt->bind_param("i", $matricule);
+            $stmt->execute();
+            $result = $stmt->get_result();
+            $user = $result->fetch_assoc();
 
-        $stmt = $conn->prepare("SELECT * FROM etudiant WHERE E_Matricule = ?");
-        $stmt->bind_param("i", $matricule);
-        $stmt->execute();
-        $result = $stmt->get_result();
-        $user = $result->fetch_assoc();
+            if ($user && password_verify($password, $user['Mot_de_passe'])) {
+                session_regenerate_id(true);
+                $_SESSION['Matricule'] = $user['E_Matricule'];
+                $_SESSION['pseudo'] = $user['Nom'];
+                $_SESSION['role'] = 'membre'; 
+                $_SESSION['logged'] = true;
 
-        if ($user && password_verify($password, $user['Mot_de_passe'])) {
-            session_regenerate_id(true);
-            $_SESSION['Matricule'] = $user['E_Matricule'];
-            $_SESSION['pseudo'] = $user['Nom'];
-            $_SESSION['role'] = 'membre'; 
-            $_SESSION['logged'] = true;
-
-            header('Location: Home.php');
-            exit();
-        } else {
-            if($j===0)
-            {
-                $erreur = "Identifiants incorrects.";
-            }
+                header('Location: Home.php');
+                exit();
+            } else{
+                    $erreur = "Identifiants non existants.";
+                }
         }
         $stmt->close();
+        
+    }
+    else{
+        $selec = "Veuillez sélectionner une session";
     }
 }
 
@@ -87,17 +92,28 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             <input type="number" name="Matricule" placeholder="Matricule" required>
             <i class='bx  bxs-user'    ></i> 
         </div> 
+        <div class="entree-login">
+            <input type="password" name="MDP" placeholder="Mot de passe" required>
+            <i class='bx  bxs-lock'    ></i> 
+        </div>
+        <div class="erreur">
             <?php if(!empty($erreur)) {?>
             <p> <?php echo $erreur; ?>
             <?php } ?>
             <?php if(!empty($attente)) {?>
             <p> <?php echo $attente; ?>
             <?php } ?>
-        <div class="entree-login">
-            <input type="password" name="MDP" placeholder="Mot de passe" required>
-            <i class='bx  bxs-lock'    ></i> 
+            <?php if(!empty($selec)) {?>
+            <p> <?php echo $selec; ?>
+            <?php } ?>
         </div>
+        <div class="groupe_radio">
+            <label for  = "Etudiant">Etudiant</label>
+            <input type = "radio" id = "Etudiant" name = "Etudiant_Responsable" value="Etudiant"> 
 
+            <label for  = "Responsable">Responsable</label>
+            <input type = "radio" id = "Responsable" name = "Etudiant_Responsable" value="Responsable"> 
+        </div>
             <button type="submit" class="bouton-login">Connexion</button>
 
         <div class="register">
